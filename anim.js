7c2a0df3b3517e9d9250e2299e114869a1e3dcbd
VenJhin <ec73784922@gmail.com>
2025-02-01 18:32:33 -0600

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,6 +1,24 @@
 // Sincronizar las letras con la canción
 var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "I like the way you sound in the morning", time: 10 },
+  { text: "We're on the phone and without a warning", time: 13 },
+  { text: "I realize you laugh is the best sound", time: 15 },
+  { text: "I have ever heard", time: 18 },
+  { text: "I like the way I can't keep my focus", time: 21 },
+  { text: "I watch you talk you didn't notice", time: 23 },
+  { text: "I hear the words but all I can think is", time: 26 },
+  { text: "We should be together", time: 29 },
+  { text: "Every time you smile, I smile", time: 32 },
+  { text: "And every tine you shine, i'll shine for you", time: 35 },
+  { text: "Whoa, oh, I'm feeling you, baby", time: 40 },
+  { text: "Don't be afraid to jump then fall", time: 44 },
+  { text: "Jump them fall into me", time: 49 },
+
+];
+
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
